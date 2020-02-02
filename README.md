fecf85c4122258b2b29fddf6fa66bf033803440e
nikhil <nikhil@rethinkworkflow.com>
2020-02-02 17:20:15 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Tesla Api
+
+this is a cloud function that enables to communicate with tesla
+
+### Prerequisites
+
+You neeed a tesla .. duh !!.
+Create a file called `credentials.js` and export username and password to your tesla account
+
+## Deployment
+
+This a firebase cloud function
+So you have can deploy your code using `firebase deploy --only functions`
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* teslajs npm https://www.npmjs.com/package/teslajs
---
+++ functions/index.js
@@ -0,0 +1,99 @@
+const functions = require('firebase-functions');
+const tjs = require('teslajs');
+const credentials = require('./credentials');
+
+ const username = credentials.USERNAME;
+const password = credentials.PASSWORD;
+
+function f2c(degf) {
+ return (degf - 32) * 5 / 9;
+}
+
+function createDelay(timeLimit) {
+ return new Promise(resolve => setTimeout(resolve, timeLimit));
+}
+
+async function setTemp(options) {
+ console.log('attempting to start climate set');
+ const climateResult = await tjs.climateStartAsync(options);
+ console.log(JSON.stringify(climateResult));
+ 
+ console.log('attempting to start temperature set');
+ const tempSetResult = await tjs.setTempsAsync(options, f2c(69), f2c(69));
+ console.log(JSON.stringify(tempSetResult));
+ 
+ console.log('attempting to start seat heater');
+ const seatHeatResult = await tjs.seatHeaterAsync(options,0,3);
+ console.log(JSON.stringify(seatHeatResult));
+}
+
+function attemptToWakeUp(options) {
+ return tjs.wakeUpAsync(options);
+}
+
+exports.teslaWarmUp = functions.https.onRequest((request, response) => {
+  tjs.login(username, password, async (err, result) => {
+   if (err) {
+    response.send("failed login!");
+   }
+   if (result.error) {
+    response.send(JSON.stringify(result.error));
+   }
+   
+   try {
+    const vehicle = await tjs.vehicleAsync({ authToken: result.authToken });
+ 
+    console.log(JSON.stringify(vehicle));
+    const options = { authToken: result.authToken, vehicleID: vehicle.id_s };
+ 
+    if (vehicle.state === 'asleep') {
+     console.log('vehicle is asleep. First attempt to wake it up');
+     const attemptOne = await attemptToWakeUp(options);
+     await createDelay(5000);
+     
+     if (attemptOne.state === 'asleep') {
+      console.log('vehicle is asleep. Second attempt to wake it up');
+      const attemptTwo = await attemptToWakeUp(options);
+      await createDelay(5000);
+      
+      if (attemptTwo.state === 'asleep') {
+       console.log('vehicle is asleep. Third attempt to wake it up');
+       const attemptThree = await attemptToWakeUp(options);
+       await createDelay(5000);
+       
+       if (attemptThree.state === 'asleep') {
+        const attemptFour = await attemptToWakeUp(options);
+        await createDelay(5000);
+ 
+        if (attemptFour.state === 'asleep') {
+         console.log('vehicle failed to wake up after four attempts');
+         return response.send(500);
+        } else {
+         await setTemp(options);
+         response.send(200);
+        }
+       } else {
+        console.log('climate set initiated');
+        await setTemp(options);
+        response.send(200);
+       }
+      } else {
+       await setTemp(options);
+       response.send(200);
+      }
+     } else {
+      await setTemp(options);
+      response.send(200);
+     }
+    } else {
+     await setTemp(options);
+     response.send(200);
+    }
+    
+   } catch(e) {
+    console.log('some error happened');
+    console.log(e);
+    response.send(500);
+   }
+  });
+});
